d12c30b177edc1591ab8660cadd92c48a8a20258
Ian Campbell <ipcampbell@gmail.com>
2022-09-23 12:11:47 -0400

    Update example to show how MyPy misses a potential AttributeError.

--- example/example.py
+++ example/example.py
@@ -3,4 +3,4 @@ from amazon_paapi.models.item_result import Item
 
 def parse_amazon_result(result: Item):
     reveal_type(result)
-    print(result.item_info.content_info)
+    print(result.item_info.content_info.publication_date)
